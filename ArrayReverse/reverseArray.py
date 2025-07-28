539cb34f0306c5445f7b2823dba72fa6d7f393ba
Nathan Maher Levy <nathanml@bu.edu>
2025-07-28 11:06:00 -0400

    Reverse array in place

--- ArrayReverse/reverseArray.py
+++ ArrayReverse/reverseArray.py
@@ -1,13 +1,34 @@
 def reverseArray(arr):
-    #Returns inputted array reversed
+    # Returns inputted array reversed
     ret = []
-    i = len(arr)-1
+    i = len(arr) - 1
     while i >= 0:
         ret.append(arr[i])
-        i = i-1
+        i = i - 1
     return ret
 
+
+def reverseArrayInPlace(arr):
+    if len(arr) == 0 or len(arr) == 1:
+        return arr
+    else:
+        left = 0
+        right = len(arr) - 1
+        while left <=  right:
+            tmp = arr[left]
+            arr[left] = arr[right]
+            arr[right] = tmp
+            right -= 1
+            left += 1
+        return arr
+
+
 # print(reverseArray([1]))
-# print(reverseArray([1,2,3,4,5]))
-# print(reverseArray([5,4,3,2,1]))
+# print(reverseArray([1, 2, 3, 4, 5]))
+# print(reverseArray([5, 4, 3, 2, 1]))
 # print(reverseArray([]))
+
+print(reverseArrayInPlace([1]))
+print(reverseArrayInPlace([1, 2, 3, 4, 5]))
+print(reverseArrayInPlace([5, 4, 3, 2, 1]))
+print(reverseArrayInPlace([]))
